adce210073c60b6d3ca8c37987cbbb337e9fd21a
digshake <dsshook@gmail.com>
2019-10-28 10:53:31 -0500

    Initial commit

---
+++ src/mousefollower/FollowTheMouse.java
@@ -0,0 +1,9 @@
+package mousefollower;
+
+public class FollowTheMouse {
+
+    public static double[] getMousePosition() {
+        //FIXME code up this method according to the extension instructions
+        return new double[0];
+    }
+}
